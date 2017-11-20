18e0fff626d05bb5e5779ca5ca1bafa9dfef6f99
Sahil Goel <sagoel@sapient.com>
2017-11-20 22:50:32 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,7 +15,15 @@ app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
   console.log("New user connected");
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text : 'Welcome to the chat app'
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text : 'New User Joined',
+    createdAt : new Date().getTime()
+  });
   socket.on("disconnect", () => {
     console.log("User Disconnected");
   });
