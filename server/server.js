a5f2dc3dae404798a3a984374cd6dc328af670cb
Sahil Goel <sagoel@sapient.com>
2017-11-20 20:38:37 +0530

    Added sockets functionality

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 var publicPath = path.join(__dirname, "../public");
 const app = express();
+
+var server  = http.createServer(app);
+var io = socketIO(server)
+
 const port = process.env.PORT || 3000;
 console.log(port);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  })
+});
+server.listen(port, () => {
   console.log(`Starting application on port ${port}`);
 });
