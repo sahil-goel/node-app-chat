0904cc3649a5d2af1bbf389d4637ebdc4e7a22a5
Sahil Goel <sagoel@sapient.com>
2017-11-22 00:11:24 +0530

    add autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = jQuery("#messages");
+  var newMessage = messages.children("li:last-child");
+  var clientHeight = messages.prop("clientHeight");
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", () => {
   console.log("New Client Connected");
 
@@ -18,6 +31,7 @@ socket.on("newMessage", (msg) => {
     text:msg.text
   });
   jQuery('#messages').append(renderedMessage);
+  scrollToBottom();
 });
 
 socket.on("locationMessage", (msg) => {
@@ -28,7 +42,8 @@ socket.on("locationMessage", (msg) => {
     formattedTime : formattedTime,
     url:msg.url
   });
-  jQuery('#locationMessages').append(renderedMessage);
+  jQuery('#messages').append(renderedMessage);
+  scrollToBottom();
 });
 
 socket.emit("createMessage", {
